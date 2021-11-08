766683edce5c6473d6d81746a5d3e9512a43836a
Josefina Andreasson <josefina.andreasson@cygni.se>
2021-11-08 21:36:05 +0100

    Unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+unregisterServiceWorker();
